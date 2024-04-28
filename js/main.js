d05d10a211c6ce3e4f87e6d880be507dc97f6115
eggirr <vlad.grishanenko.g@mail.ru>
2024-04-28 20:58:11 +0700

    Deleted unuseful things

--- js/main.js
+++ js/main.js
@@ -13,8 +13,6 @@ Vue.component('add-task', {
          <input placeholder="New task" v-model="task.title">
          </label>
         <h3>Tasks</h3>
-        <button @click="addSubtask">Add more tasks</button>
-         <button @click="delSubtask">Less tasks</button>
         <div v-for="(subtask, index) in task.subtasks"><input placeholder="Task" v-model="subtask.title" :key="index">
         </div>
         <div>
@@ -32,16 +30,6 @@ Vue.component('add-task', {
     </div>
     `,
     methods: {
-        addSubtask() {
-            if (this.task.subtasks.length < 5){
-                this.task.subtasks.push({title: "Task " + (this.task.subtasks.length + 1), done: false})
-            }
-        },
-        delSubtask(index) {
-            if(this.task.subtasks.length > 3){
-                this.task.subtasks.pop()
-            }
-        },
         addTask() {
             this.errors = [];
             if (!this.task.title || this.task.subtasks.filter(subtask => subtask.title).length < 3 || !this.task.deadline_date) {
@@ -95,10 +83,6 @@ Vue.component('column', {
         <task v-for="(task, index) in sortedTasks"
         :key="index"
         :task="task"
-        @done-subtask="doneSubtask"
-        @task-half-filled="taskHalf"
-        @task-half-filled2="taskHalf2"
-        @task-filled-completely="taskFilled"
         @del-task="delTask"
         @update-task="updateTask">
     </task>
@@ -109,18 +93,6 @@ Vue.component('column', {
         this.$emit('save')
     },
     methods: {
-        doneSubtask(subtask) {
-            this.$emit('done-subtask', subtask)
-        },
-        taskHalf(task) {
-            this.$emit('task-half-filled', {task: task, column: this.column})
-        },
-        taskHalf2(task) {
-            this.$emit('task-half-filled2', {task: task, column: this.column})
-        },
-        taskFilled(task) {
-            this.$emit('task-filled-completely', {task: task, column: this.column})
-        },
         updateTask(task) {
             this.$emit('save');
         },
@@ -143,22 +115,12 @@ Vue.component('task', {
             importance: ''
         }
     },
-    data() {
-        return {
-            newSubtaskTitle: ''
-        }
-    },
     template: `
     <div>
   <h2>{{ task.title }}</h2>
-  <li v-for="(subtask, index) in task.subtasks" class="subtask" :key="index" :class="{ done: subtask.done }" @click="doneSubtask(subtask)">
+  <li v-for="(subtask, index) in task.subtasks" class="subtask" :key="index">
     {{ subtask.title }}
-    <button v-if="!isLastColumn" @click.stop="deleteSubtask(index)">Удалить</button>
   </li>
-  <div v-if="!isLastColumn">
-    <input v-model="newSubtaskTitle" placeholder="Новая подзадача" @keyup.enter="addSubtask" />
-    <button @click="addSubtask">Добавить</button>
-  </div>
   <p>Дата изменения: {{ task.date }}</p>
   <p>Предпологаемая дата сдачи: {{ task.deadline_date }}</p>
   <p v-if="task.importance === 1">Важно</p>
@@ -166,61 +128,14 @@ Vue.component('task', {
   <button @click="delTask">Удалить задачу</button>
 </div>
     `,
-    updated() {
-        if (this.half) {
-            this.$emit('task-half-filled', this.task)
-        }
-        if (this.filled) {
-            this.$emit('task-filled-completely', this.task)
-        }
-    },
     methods: {
         delTask(task){
             this.$emit('del-task', task);
         },
-        doneSubtask(subtask) {
-            this.$emit('done-subtask', subtask)
-        },
-        addSubtask() {
-            this.task.date = new Date().getFullYear() + '-' + new Date().getMonth() + '-' + new Date().getDay()
-            if (!this.isLastColumn && this.newSubtaskTitle.trim() !== '') {
-                this.task.subtasks.push({ title: this.newSubtaskTitle.trim(), done: false });
-                this.newSubtaskTitle = '';
-                this.$emit('update-task', this.task);
-                this.$emit('task-half-filled2', this.task);
-            }
-        },
-        deleteSubtask(index) {
-            if (this.task.subtasks.length > 1) {
-                if (!this.isLastColumn) {
-                    this.task.subtasks.splice(index, 1);
-                    this.$emit('update-task', this.task);
-                    this.$emit('task-half-filled2', this.task);
-                }
-            }
-        }
     },
     computed: {
         isLastColumn() {
             return this.$parent.column.index === 3;
-        },
-        filled() {
-            let arr = []
-            this.task.subtasks.forEach(task => {
-                if (task.done === true){
-                    arr.push(task.done)
-                }
-            })
-            return (arr.length) / this.task.subtasks.length === 1
-        },
-        half() {
-            let arr = []
-            this.task.subtasks.forEach(task => {
-                if (task.done === true){
-                    arr.push(task.done)
-                }
-            })
-            return Math.ceil(this.task.subtasks.length / 2) === arr.length
         },
     }
 })
@@ -257,13 +172,6 @@ let app = new Vue({
         this.columns = JSON.parse(localStorage.getItem('columns'));
     },
     methods: {
-        taskHalf2(data) {
-            if (data.column.index === 1) {
-                if (data.task.subtasks.filter(subtask => subtask.done).length / data.task.subtasks.length < 0.5) {
-                    this.move(data, this.columns[0]);
-                }
-            }
-        },
         save() {
             localStorage.setItem('columns', JSON.stringify(this.columns))
         },
@@ -274,38 +182,9 @@ let app = new Vue({
         delTask(task){
             this.columns[0].tasks.splice(task,1)
         },
-        doneSubtask(subtask) {
-            subtask.done = true
-            this.save()
-        },
-        taskHalf(data) {
-            if (data.column.index !== 0 || data.column.disabled) return
-            if (this.columns[1].tasks.length > 4) {
-                this.columns[0].disabled = true
-                alert("Нельзя добавить ещё!")
-                return;
-            }
-            this.move(data, this.columns[1])
-        },
-        taskFilled(data) {
-            this.move(data, this.columns[2])
-            this.enabled()
-        },
-        taskTested(data){
-            this.move(data, this.columns[3])
-        },
         move(data, column) {
             data.task.date = new Date().getFullYear() + '-' + new Date().getMonth() + '-' + new Date().getDay()
             column.tasks.push(data.column.tasks.splice(data.column.tasks.indexOf(data.task), 1)[0])
         },
-        enabled() {
-            this.columns[0].disabled = false;
-            this.columns[0].tasks.forEach(task => {
-                task.subtasks = task.subtasks.filter(subtask => subtask.title);
-                if (Math.ceil(task.subtasks.length / 2) === task.subtasks.filter(subtask => subtask.done).length) {
-                    this.move({task: task, column: this.columns[0]}, this.columns[1]);
-                }
-            });
-        }
     },
 })
